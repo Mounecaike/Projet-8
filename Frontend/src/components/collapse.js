4bd03328586e22c11d95084685cb62dd7044f7ad
Mounecaike <jordanl4213@gmail.com>
2024-11-02 10:37:53 +0100

    Add files via upload
    responsive jusqu'a 767.98 px et animation des collapse en css

--- Frontend/src/components/collapse.js
+++ Frontend/src/components/collapse.js
@@ -7,30 +7,27 @@ const Collapse = ({ title, content, customClass }) => {
   const toggleCollapse = () => setIsOpen(!isOpen);
 
   return (
-    <div className={`collapse-container ${customClass || ''}`}>
+    <div className={`collapse-container ${isOpen ? 'open' : ''} ${customClass || ''}`}>
       <div className="collapse-bar" onClick={toggleCollapse}>
-        <h2 className={`collapse-title ${customClass || ''}`}>{title}</h2>
+        <h2 className="collapse-title">{title}</h2>
         <img
           src={require('../Assets/collapse/arrow-up.png')}
           alt="Flèche pour ouvrir/fermer"
           className={`collapse-arrow ${isOpen ? 'rotate' : ''}`}
         />
       </div>
-      {isOpen && (
-        <div className="collapse-content">
-          {Array.isArray(content) ? (
-            <ul>
-              {content.map((item, index) => (
-                <li key={index}>{item}</li>
-              ))}
-            </ul>
-          ) : (
-            <p>{content}</p>
-          )}
-        </div>
-      )}
+      <div className="collapse-content">
+        {Array.isArray(content) ? (
+          <ul>
+            {content.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        ) : (
+          <p>{content}</p>
+        )}
+      </div>
     </div>
   );
-};
-
+}  
 export default Collapse;
